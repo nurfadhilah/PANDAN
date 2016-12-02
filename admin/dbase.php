05eb6f013d2d11b3f20c5b2f30d08c19cfdc6c03
nurfadhilah <ellah.fadhilah93@gmail.com>
2016-12-02 21:49:51 +0800

    Friday 02-Dec-16  9:49:49 PM

---
+++ admin/dbase.php
@@ -0,0 +1,18 @@
+<!-- dbaseStd.php -->
+<!-- To connect between php scripting and database. -->
+<?php
+
+//To establish a connection to database and save in $conn
+$conn = mysql_connect('localhost', 'root', '', 'pandan');
+//if connection failed then display mysql error
+if (!$conn){
+die("Could not connect to database");
+}
+
+//To select one particular database to be used
+mysql_select_db("pandan", $conn) or die("Could not open product database");
+
+//set the default time zone to use in Malaysia
+date_default_timezone_set('Asia/Kuala_Lumpur');
+?>
+
